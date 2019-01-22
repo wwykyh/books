30cabf304cdeb418ce26b022f47198ea3899a820
wwykyh <952982609@qq.com>
2019-01-22 09:58:19 +0800

    添加注释

--- BOOK/src/main/java/com/dragon/controller/BookController.java
+++ BOOK/src/main/java/com/dragon/controller/BookController.java
@@ -1,3 +1,16 @@
+/**
+* 版权所有：厦门市巨龙软件工程有限公司
+* Copyright 2009 Xiamen Dragon Software Eng. Co. Ltd.
+* All right reserved. 
+*====================================================
+* 文件名称: BookController.java
+* 修订记录：
+* No    日期				    作者(操作:具体内容)
+* 1.    1 22, 2019			王炜焱(创建:创建文件)
+*====================================================
+* 类描述：图书控制器
+* 
+*/
 package com.dragon.controller;
 
 import org.springframework.stereotype.Controller;
--- BOOK/src/main/java/com/dragon/mapper/BookMapper.java
+++ BOOK/src/main/java/com/dragon/mapper/BookMapper.java
@@ -1,3 +1,17 @@
+/**
+* 版权所有：厦门市巨龙软件工程有限公司
+* Copyright 2009 Xiamen Dragon Software Eng. Co. Ltd.
+* All right reserved. 
+*====================================================
+* 文件名称: BookMapper.java
+* 修订记录：
+* No    日期				    作者(操作:具体内容)
+* 1.    1 22, 2019			王炜焱(创建:创建文件)
+*====================================================
+* 类描述：图书映射接口
+* 
+*/
+
 package com.dragon.mapper;
 
 public class BookMapper {
