bb92efa41da4944404bb50383aeb814868b290d0
ll4285 <ll4285@126.com>
2019-01-23 21:10:31 +0800

    添加图书管理模块功能页面

--- BOOK/src/main/java/com/dragon/book/controller/BookManagerController.java
+++ BOOK/src/main/java/com/dragon/book/controller/BookManagerController.java
@@ -1,4 +1,4 @@
-package com.dragon.controller;
+package com.dragon.book.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
