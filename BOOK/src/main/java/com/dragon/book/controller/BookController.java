79d9b5bc7284c76c5420504eba0b88d60adb0caa
wwykyh <952982609@qq.com>
2019-01-24 10:42:53 +0800

    测试

--- BOOK/src/main/java/com/dragon/book/controller/BookController.java
+++ BOOK/src/main/java/com/dragon/book/controller/BookController.java
@@ -13,8 +13,12 @@
 */
 package com.dragon.book.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class BookController {
@@ -24,14 +28,12 @@ public class BookController {
 	}
 	
 	@GetMapping("/adminindex")
-	public String adminbookIndex() throws Exception{
+	public String adminBookIndex() throws Exception{
 		return "adminIndex";
 	}
-	
-	
-	
-	@GetMapping("/user")
-	public String user() throws Exception{
-		return "user";
+	@GetMapping("/login")
+	public String login() throws Exception{
+		return "login";
 	}
+	
 }
