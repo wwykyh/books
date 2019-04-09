b3eee7d1d594364dc0885557f1e22d8878954540
zhouzhoumao <zhouzhaomao@qq.com>
2019-04-09 13:40:56 +0800

    程序入口加注入注解@Configuration

--- BOOK/src/main/java/com/dragon/book/BookApplication.java
+++ BOOK/src/main/java/com/dragon/book/BookApplication.java
@@ -3,8 +3,10 @@ package com.dragon.book;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@Configuration
 @EnableScheduling //定時任务注解
 @SpringBootApplication
 @MapperScan("com.dragon.book.mapper")
